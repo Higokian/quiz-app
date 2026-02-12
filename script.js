8292d7c7449e9fe28d6cbaf0fa018171faf1d7d2
Jason <jason_ritter@outlook.com>
2026-02-12 15:53:10 -0600

    Set up question data so I can dive into logic tomorrow

---
+++ script.js
@@ -0,0 +1,55 @@
+// Quiz data
+const quizData = [
+    {
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Markup Language",
+            "Hight Tech Modern Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ],
+        correct: 0
+    },
+    {
+        question: "Which is NOT a javascript data type?",
+        options: ["String", "Boolean", "Float", "Undefined"],
+        correct: 2
+    },
+    {
+        question: "What does CSS stand for?",
+        options: [
+            "Computer Style Sheets",
+            "Cascading Style Sheets",
+            "Creative Style Sheets",
+            "Color Style Sheets"
+        ],
+        correct: 1
+    },
+    {
+        question: "Which method is used to add and element at the end of an array?",
+        options: ["push()", "pop()", "shift()", "unshift()"],
+        correct: 0
+    },
+    {
+        question: "What is the correct way to declare a JavaScript variable?",
+        options: ["var myVar;", "variable myVar;", "v myVar;", "declare myVar;"],
+        correct: 0
+    }
+];
+
+// TODO: Select DOM elements
+// We'll do this tomorrow!
+
+// TODO: Track quiz state
+// currentQuestion, score, etc.
+
+// TODO: Load question function
+
+// TODO: Check answer function
+
+// TODO: Show result function
+
+// TODO: Restart quiz function
+
+console.log('Quiz app loaded!');
+console.log('Quiz data:', quizData);
